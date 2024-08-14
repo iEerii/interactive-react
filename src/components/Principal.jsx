ecc63b4d80a563a95e8de897db9986388ce47c0e
Ericka Ponce <erickaponce92@gmail.com>
2024-08-14 15:20:31 -0500

    add principal file

---
+++ src/components/Principal.jsx
@@ -0,0 +1,25 @@
+export const Principal = () => {
+    return(
+        <main className="mainContainer">
+            <div>
+                <img src="" alt="" />
+                <h1>How did we do?</h1>
+                <p>
+                    Please let us know how we did with your support request. All feedback is appreciated to helps us improve our offering!
+                </p>
+            </div>
+            <div>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>5</li>
+                    <li>6</li>
+                </ul>
+            </div>
+            <div>
+                <button>Submit</button>
+            </div>
+        </main>
+    )
+}
