f90678bd7c9d4aefb56374eb09539fb874924cfb
Ericka Ponce <erickaponce92@gmail.com>
2024-08-19 19:36:53 -0500

    change styles to buttons

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,12 +1,19 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react";
 import './../styles/survey.css'
 
 export const Survey = () => {
+    const[rating, setRating] = useState(null);
     const navigate = useNavigate();
 
-    const handleSubmit = (opcion) => {
-        if (opcion !== null) {
-            navigate(`/thank-you/${opcion}`)
+    const handleRatingClick = (newRating) => {
+        setRating(newRating);
+    };
+
+    const handleSubmit = () => {
+        if (rating !== null) {
+            navigate(`/thank-you/${rating}`)
+            console.log(`El numero ${rating} ha sido elegido`)
         }
     }
 
@@ -15,12 +22,18 @@ export const Survey = () => {
             <ul className='surveyNumber'>
                     {[1, 2, 3, 4, 5].map((opcion) => {
                         return <li key={opcion}>
-                            <button className="btnOption">{opcion}</button> 
+                            <button 
+                                type="button"
+                                className={`btnOption ${rating === opcion ? 'active' : ''}`}
+                                onClick={() => handleRatingClick(opcion)}
+                                >
+                                {opcion}
+                            </button> 
                         </li>
                     })
                 }
             </ul>
-            <button className="btnSubmit" onClick={() => handleSubmit(opcion)}>
+            <button type="button" className="btnSubmit" onClick={handleSubmit} disabled={rating === null}>
                 Submit
             </button>
         </div>      
